48092dfa88cbdcc568f53d089adc8020716ab882
[Sona] <[sona16061999@gmail.com]>
2021-07-12 23:14:40 +0530

    [Sona] Permutation of a given string using recursion

---
+++ AlgorithmPrograms/Permutation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class Permutation
+    {
+        public static void Permute(String str,
+                                int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Permute(str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+
+        /**
+        * Swap Characters at position
+        * @param a string value
+        * @param i position 1
+        * @param j position 2
+        * @return swapped string
+        */
+        public static String Swap(String a,
+                                  int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        // Driver Code
+        
+    }
+}
